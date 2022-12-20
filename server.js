c1872f3399d543970088d6cc558c5e8651a65eb8
Anastasia Urusova <anastasiya.urusova@stud.fh-campuswien.ac.at>
2022-12-20 20:52:46 +0100

    - setup angular - setup node as webserver

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3080
-//const dist = process.cwd()+"/book-tracker-ui/dist/book-tracker/"
+const dist = process.cwd()+"/book-tracker-ui/dist/book-tracker-ui/"
 
 app.use(express.json())
-//app.use(express.static(dist));
+app.use(express.static(dist));
 
-/*
+
 app.get('/', (req,res) => {
-    res.sendFile(dist);
+    res.sendFile(dist+"index.html");
 });
 
- */
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
