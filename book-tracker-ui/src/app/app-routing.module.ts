a33adcd860b796d9ac38f2e0efa34208acdae4bd
Anastasia Urusova <anastasiya.urusova@stud.fh-campuswien.ac.at>
2023-01-18 19:36:20 +0100

    Added new components and changed the landing page

--- book-tracker-ui/src/app/app-routing.module.ts
+++ book-tracker-ui/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./auth/login/login.component";
+import {RegistrationComponent} from "./auth/registration/registration.component";
+import {HomepageComponent} from "./homepage/homepage.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'home', component: HomepageComponent},
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- book-tracker-ui/src/app/app.module.ts
+++ book-tracker-ui/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookListComponent } from './book-list/book-list.component';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    RegistrationComponent,
+    LoginComponent,
+    AccountSettingsComponent,
+    BookDetailsComponent,
+    BookListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
