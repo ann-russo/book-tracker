3337a8137df6800e838a8f94db0855babd6b1a21
Anastasia Urusova <anastasiya.urusova@stud.fh-campuswien.ac.at>
2023-03-02 14:40:33 +0100

    trying changes for deployment

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3080
 const dist = process.cwd()+"/public"
 
 const bookRouter = require ('./bookapi/routes/book-router');
 const userRouter = require ('./bookapi/routes/user-routes');
 const booklistRouter = require ('./bookapi/routes/booklist-router');
+
 /*
  * mongodb configuration
  */
@@ -13,26 +13,32 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
+const mongodbUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-tracker';
+
+console.log(process.env)
+
+mongoose.set('strictQuery', false);
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('Connected to the database')
+})
 
 app.use(express.json())
 app.use(express.static(dist));
-
 //include routes for user api
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.set('page-size', '20');
-    res.set('Access-Control-Expose-Headers', 'page-size')
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
 });
-
-app.use(cors({
-    credentials: true,
-    origin: ['http://localhost:3080', 'http://localhost:4200', 'http://localhost:8080', 'http://localhost:80', 'http://127.0.0.1:3080']
-}))
 
 app.use('/api/users', userRouter); //TODO during implementation add registered info when required...
 app.use('/api/booklist', booklistRouter);
@@ -42,42 +48,11 @@ app.get('/', (req,res) => {
     res.sendFile(dist+"index.html");
 });
 
-/**
- * Redirect to index.html
- */
 app.route('/*').get(function (req, res) {
     return res.sendFile(path.join(dist + 'index.html'));
 });
 
-app.listen(port, (error)=> {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Server is up on port ' + port)
-    }
-})
-
-const mongodbUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-tracker';
-/*
-mongoose.set('strictQuery', false);
-mongoose.connect(mongodbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, () => {
-    console.log('Connected to the database')
-})
-
- */
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const client = new MongoClient(mongodbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1
-});
-client.connect(err => {
-    const collection = client.db("book-tracker").collection("users");
-    console.log('Connected to the database')
-    // perform actions on the collection object
-    client.close();
+const port = process.env.PORT || 3080
+app.listen(port, () => {
+    console.log("Listening on port " + port);
 });
